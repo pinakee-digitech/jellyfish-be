b7dc6cf3104fc257a5e922af20abb9d1f2b3b1a5
v4rdhan <103747751+v4rdhan@users.noreply.github.com>
2024-10-22 21:31:22 +0530

    created broiler code in java

---
+++ src/main/java/com/jellyFish/be/JellyFish/JellyFishApplication.java
@@ -0,0 +1,13 @@
+package com.jellyFish.be.JellyFish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JellyFishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JellyFishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jellyFish/be/JellyFish/JellyFishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jellyFish.be.JellyFish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JellyFishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
